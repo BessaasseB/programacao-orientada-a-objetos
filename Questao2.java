65f9533a5d8a2d3e012510d6c8edc965cd385a8c
João Victor Bessa <93410184+BessaasseB@users.noreply.github.com>
2023-04-10 11:00:20 -0300

    Add files via upload

---
+++ Questao2.java
@@ -0,0 +1,17 @@
+package questao2;
+
+import java.util.Scanner;
+
+public class Questao2 {
+
+    public static void main(String[] args) {
+        String[] pratoNome = {"Vegetariano", "Peixe", "Frango", "Carne"};
+        int[] prato = {180, 230, 250, 350};
+        String[] sobremesaNome = {"Abacaxi", "Sorvete Diet", "Mousse Diet", "Mousse de Chocolate"};
+        int[] sobremesa = {75, 110, 170, 200};
+        String[] bebidaNome = {"Cha", "Suco de laranja", "Suco de melao", "Refrigerante Diet"};
+        int[] bebida = {20, 70, 100, 65};
+              
+    }
+    
+}
