f0a94ec803150773a24cccbc68d5afd358f5383c
João Victor Bessa <93410184+jvBessa@users.noreply.github.com>
2023-05-14 22:33:16 -0300

    Add files via upload

---
+++ Exercicios complementares/ExercicioComplementar2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Restaurante r = new Restaurante();
+        
+        r.insert();
+        System.out.println("\n");
+        r.print();
+    }
+    
+}
